3bb93a62460d8e58f5939285496153e52acd1563
Susant Paudel <techbizznepal@gmail.com>
2022-08-14 20:09:55 +0545

    Update WorkProcessComponent.tsx

--- components/home/WorkProcessComponent.tsx
+++ components/home/WorkProcessComponent.tsx
@@ -37,7 +37,7 @@ const WorkProcessComponent: FC<WorkProcessComponentProps> = ({heading, processes
         <div className={`bg-blue-100`}>
             <div className={`flex flex-col gap-y-16 py-32 container mx-auto`}>
                 <div className={`py-4`}><h2 className={`text-center text-xl uppercase`}>{heading}</h2></div>
-                <div className={`flex justify-evenly w-full flex-wrap mt-5`}>
+                <div className={`grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5  gap-y-16 mt-5 `}>
                     {processes.map((item) =>
                         <SingleProcessComponent
                             key={item.slug}
