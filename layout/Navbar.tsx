70896aee047ffbe8c6d71e64bd79a986eb6c5469
Susant Paudel <techbizznepal@gmail.com>
2022-08-15 11:25:26 +0545

    Update Navbar.tsx

--- layout/Navbar.tsx
+++ layout/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar(): JSX.Element {
     const {headerInfo, navigationItems, logo} = useContentSchema()
     return (
         <div className={`sticky top-0  bg-white bg-opacity-50 z-10 shadow-sm`}>
-            <div className={`container mx-auto w-full flex justify-between flex items-center `}>
+            <div className={`container mx-auto w-full flex justify-between flex items-center py-1 md:py-4`}>
                 <Logo title={logo.title} imagePath={logo.imagePath}/>
                 <HeaderInfo email={headerInfo.email} phone={headerInfo.phone}/>
                 {/*<NavigationItems items={navigationItems}/>*/}
